e67d2f5b2344bcbc1c4e38022c722417f302ab07
DeltaV3 <cristiand1346@gmail.com>
2025-03-01 12:33:11 -0500

    Implemented PostMapping AND RequestBody

--- Spring Boot/cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
+++ Spring Boot/cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
@@ -1,8 +1,7 @@
 package cursoSpringBoot.controllers;
 
 import cursoSpringBoot.domain.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,8 +18,29 @@ public class CustomerController {
             new Customer(234,"Carlos Martinez", "carlosm", "password234")
     ));
 
-    @GetMapping("clientes")
+
+    //Obtiene los clientes que hay en la aplicaciones
+    @GetMapping("/clientes")
     public List<Customer> getCustomers() {
         return customers;
     }
+
+    //Obtiene solo un cliente en especifico
+    @GetMapping("/clientes/{username}")
+    public Customer getCustomer(@PathVariable String username) {
+        for (Customer c : customers) {
+            if (c.getUsername().equalsIgnoreCase(username)) {
+                return c;
+            }
+        }
+        return null; //Mala practica, hay que mostrar un error 404
+    }
+
+    // Metodo para agregar mas clientes
+    @PostMapping("/clientes")
+    public Customer postCliente(@RequestBody Customer customer) {
+        customers.add(customer);
+        return customer;
+    }
+
 }
